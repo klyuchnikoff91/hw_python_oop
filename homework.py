e1381169b62167723eee1d06f35715e793ad852f
Klyuchnikov <klyuchnik.jenia@yandex.ru>
2021-09-12 12:06:05 +0500

    Подогнал под автотесты

--- homework.py
+++ homework.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 
 class Calculator:
+    """Шаблонный калькулятор."""
     def __init__(self, limit: float):
         self.limit = limit
         self.records: list[object] = list()
@@ -13,62 +14,70 @@ class Calculator:
         today = dt.date.today()
         today_amount = 0
         for r in self.records:
-            if (dt.date(r.timestamp) == today):
+            if (r.date == today):
                 today_amount += r.amount
         return today_amount
 
     def get_week_stats(self):
-        today = dt.datetime.now()
-        start_dow = dt.date(today - dt.timedelta(7))
+        today = dt.date.today()
+        start_dow = today - dt.timedelta(days=7)
         week_amount = 0
         for r in self.records:
-            if ((r.timestamp <= today) and (r.timestamp >= dt.datetime(start_dow))):
+            if ((r.date <= today) and (r.date > start_dow)):
                 week_amount += r.amount
         return week_amount
 
 
 class Record:
+    """Структура, обрабатывающая и хранящая записи калькуляторов.
+    Проверяет ошибки при вводе."""
     def __init__(self,
                  amount: float,
                  comment: str,
-                 timestamp = dt.datetime.now()) -> None:
+                 date = None) -> None:
         try:
-            self.amount = float(amount)
-            self.comment = str(comment)
             date_format = '%d.%m.%Y'
-            if (type(timestamp) is str):
-                self.timestamp = dt.datetime.strptime(timestamp, date_format)  # Парсим строку в заданный формат
-            elif isinstance(timestamp, dt.datetime):
-                self.timestamp = timestamp
+            if (type(date) is str):
+                self.date = dt.datetime.strptime(date, date_format).date()
+            elif isinstance(date, dt.date):
+                self.date = date
+            elif date is None:
+                self.date = dt.date.today()
             else:
-                raise ValueError  # Показываем ошибку, другие форматы не принимаем
+                raise ValueError
+            self.amount = float(amount)
+            self.comment = comment
         except ValueError:
-            print('Запись не верна')
+            print("Запись не верна")
+            pass
 
 
 class CashCalculator(Calculator):
-    def get_today_cash_remained(currency):
-        USD_RATE = 73.13
-        EUR_RATE = 86.47
-        balance = super().limit - super().get_today_stats()
+    """Ещё один скучный калькулятор"""
+    USD_RATE = 73.13
+    EURO_RATE = 86.47
+
+    def get_today_cash_remained(self, currency):
+        balance = self.limit - self.get_today_stats()
         format_str_currency = "руб"
         if (currency == 'usd'):
-            balance *= USD_RATE
-            format_str_currency = "USD"
+            balance = balance / self.USD_RATE
+            format_str_currency = currency.upper()
         elif (currency == 'eur'):
-            balance *= EUR_RATE
+            balance = balance / self.EURO_RATE
             format_str_currency = "Euro"
         if balance > 0:
-            return f"На сегодня осталось {balance} {format_str_currency}"
+            return f"На сегодня осталось {round(balance, 2)} {format_str_currency}"
         elif balance == 0:
             return "Денег нет, держись"
         else:
-            return f"Денег нет, держись: твой долг - {balance} {format_str_currency}"
+            return f"Денег нет, держись: твой долг - {-round(balance, 2)} {format_str_currency}"
 
 
 class CaloriesCalculator(Calculator):
-    def get_calories_remained():
-        balance = super().limit - super().get_today_stats()
+    """Опять калькулятор."""
+    def get_calories_remained(self):
+        balance = round(self.limit - self.get_today_stats())
         if (balance > 0):
             return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал"
         else:
