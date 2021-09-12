3422e4982c162d36663e63b7b44d014d3632f9ad
Klyuchnikov <klyuchnik.jenia@yandex.ru>
2021-09-12 13:00:13 +0500

    Final touches

--- homework.py
+++ homework.py
@@ -5,23 +5,21 @@ class Calculator:
     """Шаблонный калькулятор."""
     def __init__(self, limit: float):
         self.limit = limit
-        self.records: list[object] = list()
+        self.records: list[Record] = list()
 
     def add_record(self, record: object):
         self.records.append(record)
 
-    def get_today_stats(self):
+    def get_today_stats(self) -> float:
         today = dt.date.today()
         today_amount = sum(r.amount for r in self.records if r.date == today)
         return today_amount
 
-    def get_week_stats(self):
+    def get_week_stats(self) -> float:
         today = dt.date.today()
         start_dow = today - dt.timedelta(days=7)
-        week_amount = 0
-        for r in self.records:
-            if ((r.date <= today) and (r.date > start_dow)):
-                week_amount += r.amount
+        week_amount = sum(r.amount for r in self.records
+                          if (r.date <= today) and (r.date > start_dow))
         return week_amount
 
 
@@ -31,7 +29,7 @@ class Record:
     def __init__(self,
                  amount: float,
                  comment: str,
-                 date = None) -> None:
+                 date=None) -> None:
         try:
             date_format = '%d.%m.%Y'
             if (type(date) is str):
@@ -54,7 +52,7 @@ class CashCalculator(Calculator):
     USD_RATE = 72.76
     EURO_RATE = 86.15
 
-    def get_today_cash_remained(self, currency):
+    def get_today_cash_remained(self, currency: str) -> str:
         balance = self.limit - self.get_today_stats()
         format_str_currency = "руб"
         if (currency == 'usd'):
@@ -64,18 +62,27 @@ class CashCalculator(Calculator):
             balance = balance / self.EURO_RATE
             format_str_currency = "Euro"
         if balance > 0:
-            return f"На сегодня осталось {round(balance, 2)} {format_str_currency}"
+            return (
+                "На сегодня осталось "
+                f"{round(balance, 2)} {format_str_currency}"
+            )
         elif balance == 0:
             return "Денег нет, держись"
         else:
-            return f"Денег нет, держись: твой долг - {-round(balance, 2)} {format_str_currency}"
+            return (
+                "Денег нет, держись: твой долг - "
+                f"{-round(balance, 2)} {format_str_currency}"
+            )
 
 
 class CaloriesCalculator(Calculator):
     """Опять калькулятор."""
-    def get_calories_remained(self):
+    def get_calories_remained(self) -> str:
         balance = round(self.limit - self.get_today_stats())
         if (balance > 0):
-            return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал"
+            return (
+                "Сегодня можно съесть что-нибудь ещё, но с общей калорийностью"
+                f" не более {balance} кКал"
+            )
         else:
             return "Хватит есть!"
