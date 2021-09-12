0ea8f8070f3d9ede6d927c5e7c7b3f079e00d43b
Klyuchnikov <klyuchnik.jenia@yandex.ru>
2021-09-13 03:46:22 +0500

    Waste of time

--- homework.py
+++ homework.py
@@ -2,84 +2,79 @@ import datetime as dt
 
 
 class Calculator:
-    """Шаблонный калькулятор."""
+    """Калькулятор."""
     def __init__(self, limit: float):
         self.limit = limit
-        self.records: list[Record] = list()
+        self.records = list()
 
     def add_record(self, record: object):
         self.records.append(record)
 
-    def get_today_stats(self) -> float:
+    def get_today_stats(self):
         today = dt.date.today()
-        today_amount = sum(r.amount for r in self.records if r.date == today)
+        today_amount = sum(iter.amount for iter in self.records
+                           if iter.date == today)
         return today_amount
 
-    def get_week_stats(self) -> float:
+    def get_week_stats(self):
         today = dt.date.today()
         start_dow = today - dt.timedelta(days=7)
-        week_amount = sum(r.amount for r in self.records
-                          if (r.date <= today) and (r.date > start_dow))
+        week_amount = sum(iter.amount for iter in self.records
+                          if start_dow < iter.date <= today)
         return week_amount
+
+    def get_balance(self):
+        return self.limit - self.get_today_stats()
 
 
 class Record:
-    """Структура, обрабатывающая и хранящая записи калькуляторов.
-    Проверяет ошибки при вводе."""
+    """Структура, хранящая записи калькуляторов."""
     def __init__(self,
-                 amount: float,
-                 comment: str,
-                 date=None) -> None:
-        try:
-            date_format = '%d.%m.%Y'
-            if (type(date) is str):
-                self.date = dt.datetime.strptime(date, date_format).date()
-            elif isinstance(date, dt.date):
-                self.date = date
-            elif date is None:
-                self.date = dt.date.today()
-            else:
-                raise ValueError
-            self.amount = float(amount)
-            self.comment = comment
-        except ValueError:
-            print("Запись не верна")
-            pass
+                 amount,
+                 comment,
+                 date=None):
+        date_format = '%d.%m.%Y'
+        if (date is None):
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, date_format).date()
+        self.amount = amount
+        self.comment = comment
 
 
 class CashCalculator(Calculator):
-    """Ещё один скучный калькулятор"""
+    """Ещё калькулятор"""
     USD_RATE = 72.76
     EURO_RATE = 86.15
+    CURRENCY_RATE = {
+        'rub': (1, 'руб'),
+        'usd': (USD_RATE, 'USD'),
+        'eur': (EURO_RATE, 'Euro'),
+    }
 
-    def get_today_cash_remained(self, currency: str) -> str:
-        balance = self.limit - self.get_today_stats()
-        format_str_currency = "руб"
-        if (currency == 'usd'):
-            balance = balance / self.USD_RATE
-            format_str_currency = currency.upper()
-        elif (currency == 'eur'):
-            balance = balance / self.EURO_RATE
-            format_str_currency = "Euro"
+    def get_today_cash_remained(self, currency):
+        balance = self.get_balance()
+        balance = round(balance / self.CURRENCY_RATE[currency][0], 2)
         if balance > 0:
             return (
                 "На сегодня осталось "
-                f"{round(balance, 2)} {format_str_currency}"
+                f"{balance} {self.CURRENCY_RATE[currency][1]}"
             )
         elif balance == 0:
             return "Денег нет, держись"
         else:
+            balance = abs(balance)
             return (
                 "Денег нет, держись: твой долг - "
-                f"{-round(balance, 2)} {format_str_currency}"
+                f"{balance} {self.CURRENCY_RATE[currency][1]}"
             )
 
 
 class CaloriesCalculator(Calculator):
     """Опять калькулятор."""
-    def get_calories_remained(self) -> str:
-        balance = round(self.limit - self.get_today_stats())
-        if (balance > 0):
+    def get_calories_remained(self):
+        balance = self.get_balance()
+        if balance > 0:
             return (
                 "Сегодня можно съесть что-нибудь ещё, но с общей калорийностью"
                 f" не более {balance} кКал"
